0388532cd0a8ea4427c638640283b8eacf782ce4
Valeh Huseynov <valehmh@code.edu.az>
2026-02-10 14:41:52 +0400

    make: App configuration and joi package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,43 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { BooksModule } from './books/books.module';
 import { Book } from './books/entities/books.entity';
 import { AuthorsModule } from './authors/authors.module';
 import { Author } from './authors/entities/author.entity';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: '',
-      password: '',
-      database: 'book_database',
-      entities: [Book, Author],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}.local`,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required().default(5432),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_SYNCHRONIZE: Joi.bool().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: Number(configService.get<number>('DATABASE_PORT')),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Book, Author],
+        synchronize: Boolean(configService.get<string>('DATABASE_SYNCHRONIZE')),
+      }),
     }),
     BooksModule,
     AuthorsModule,
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
 import { Book } from './entities/books.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([Book])],
   controllers: [BooksController],
   providers: [BooksService],
 })
